d83e61220047a67323fe3a01b05ccbc11d3891e6
SerhiiMolodets <mr.molodets@gmail.commr.molodets@gmail.com>
2022-09-23 15:59:20 +0300

    project added;

---
+++ TheMovieDBapp/TheMovieDBapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDBapp
+//
+//  Created by Сергей Молодец on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
